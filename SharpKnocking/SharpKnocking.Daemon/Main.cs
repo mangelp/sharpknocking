b9bd58b8dae38cbf83f47ac71f0fa882de15d182
mangelp <mangelp@26a6a90b-202d-0410-bbe6-95a292849a38>
2007-09-24 08:20:17 +0000

    Some new projects, minor changes in the IptablesNet solution and some file changes (new ones and removed ones).

---
+++ SharpKnocking/SharpKnocking.Daemon/Main.cs
@@ -0,0 +1,17 @@
+// /home/mangelp/Projects/sharpknocking/SharpKnocking/SharpKnocking.Daemon/Main.cs created with MonoDevelop at 17:20 14/06/2007 by mangelp 
+//
+//This project is released under the terms of the LGPL V2. See the file lgpl.txt for details.
+//(c) 2007 SharpKnocking projects and authors (see AUTHORS).
+// project created on 14/06/2007 at 17:20
+using System;
+
+namespace SharpKnocking.Daemon
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
