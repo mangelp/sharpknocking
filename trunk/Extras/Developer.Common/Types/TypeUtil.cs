5a7bae4135c35af0b9ed2c99e64b2a1991bbb4cd
mangelp <mangelp@26a6a90b-202d-0410-bbe6-95a292849a38>
2007-10-29 07:50:43 +0000

    Added an utility class to load assemblies. The behavior of Assembly.LoadFrom() looks different from the one of .Net, this could be a bug or a mismatch in my memory :p, we should check it and report.

--- trunk/Extras/Developer.Common/Types/TypeUtil.cs
+++ trunk/Extras/Developer.Common/Types/TypeUtil.cs
@@ -15,28 +15,61 @@ namespace Developer.Common.Types
         /// </summary>
         /// <remarks>
         /// The value parameter can be a string with the numeric value  to
-        /// check, a string with the name of the enumeration or a number.
+        /// check, an string with the name of the enumeration or a number.
         /// </remarks>
 		public static bool IsEnumValue(Type enumType, string enumValue)
 		{
-            return (TypeUtil.GetEnumValue(enumType, enumValue)!=null);		        
+			object obj;
+            return TryGetEnumValue(enumType, enumValue, out obj);		        
+		}
+		
+		/// <summary>
+		/// Tries to convert a value to an enumeration const
+		/// </summary>
+		public static bool TryGetEnumValue(Type enumType, object value, out object enumConst)
+		{
+			enumConst = null;
+		    if(value==null)
+		        return false;
+			
+			if(value is string || enumConst.GetType().IsValueType)
+			{
+				string strVal = value.ToString();
+				try {
+					enumConst = Enum.Parse(enumType, strVal, true);
+					//Console.WriteLine("Got enum "+enumConst+" from "+value+" as "+value.GetType().Name);
+				} catch (Exception ex) {
+					//Console.Out.WriteLine("\n-- Exception detected: "+ex.Message+" --\n"+ex+"\n-- End of exception --\n");
+					return false;
+				}
+				return true;
+			}
+			else if(Enum.IsDefined(enumType, value))
+			{
+				enumConst = Enum.ToObject(enumType, value);
+				Console.WriteLine("Got enum "+enumConst+" from "+value+" as "+value.GetType().Name);
+				return true;
+			}
+			
+			return false;
 		}
 		
 		/// <summary>
 		/// Returns the enum member that represents the value if it is valid
 		/// </summary>
+		/// <remarks>
+		/// If the value can't be converted to an enum value this returns null.
+		/// </remarks>
 		public static object GetEnumValue(Type enumType, object value)
 		{
 		    if(value==null)
 		        return null;
-		        
+					        
 		    if(value.GetType() == Enum.GetUnderlyingType(enumType) &&
-		                  !Enum.IsDefined(enumType, value))
-		    {
+		                  !Enum.IsDefined(enumType, value)) {
 		        return null;
 		    }
 		    
-
 		    string strVal = value.ToString().Trim();
 		    return Enum.Parse(enumType, strVal, true);
 		}
@@ -67,8 +100,5 @@ namespace Developer.Common.Types
 		    
 		    return null;
 		}
-		
-		
-		
 	}
 }
