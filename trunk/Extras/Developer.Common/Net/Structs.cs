93c942a5485ef5c20a09b6afc7917d6dc8101a44
mangelp <mangelp@26a6a90b-202d-0410-bbe6-95a292849a38>
2007-11-05 17:36:23 +0000

    - Updated license headers - Removed Ipv4 structure - Changed version

--- trunk/Extras/Developer.Common/Net/Structs.cs
+++ trunk/Extras/Developer.Common/Net/Structs.cs
@@ -11,259 +11,259 @@ using System.Collections.Generic;
 
 namespace Developer.Common.Net
 {
-	/// <summary>
-	/// Raw data struct for ipv4. Stores ip and mask data.
-	/// </summary>
-	public struct IPv4RawAddress
-	{
-		/// <summary>
-		/// Defines a IPv4RawAddress with all the bytes set to zero.
-		/// </summary>
-		public static readonly IPv4RawAddress Zero = new IPv4RawAddress(new byte[]{0,0,0,0},new byte[]{0,0,0,0});
-		
-		/// <summary>
-		/// IP address (4 bytes)
-		/// </summary>
-		private byte[] address;
-		
-		/// <summary>
-		/// Gets/Sets the address bytes (4 bytes).
-		/// </summary>
-		public byte[] Address
-		{
-			get {return this.address;}
-			set {
-				
-				if(value.Length!=4)
-					throw new ArgumentException("The address must be 4 bytes long","value");
-				
-				this.address = value;
-			}
-		}
-		
-		/// <summary>
-		/// Mask (4bytes)
-		/// </summary>
-		private byte[] mask;
-		
-		/// <summary>
-		/// Gets/Sets the mask bytes (4 bytes)
-		/// </summary>
-		public byte[] Mask
-		{
-			get {return this.mask;}
-			set {
-				
-				if(value.Length!=4)
-					throw new ArgumentException("The mask must be 4 bytes long","value");
-				
-				this.mask = value;
-			}
-		}
-		
-		/// <summary>
-		/// Returns if there is a valid mask defined
-		/// </summary>
-		public bool HasMask
-		{
-			get { return Mask[3]==0 && Mask[2]==0 && Mask[1]==0 && Mask[0]==0;}
-		}
-		
-		/// <summary>
-		/// Initializes the struct with the bytes for the address.
-		/// </summary>
-		public IPv4RawAddress(byte[] addr)
-		{
-			if(addr.Length!=4)
-				throw new ArgumentException("The array must have 4 bytes","addr");
-			this.Address = addr;
-			this.Mask = new byte[4]{0,0,0,0};
-		}
-		
-		/// <summary>
-		/// Initializes the struct with the bytes for the address and the mask
-		/// </summary>
-		public IPv4RawAddress(byte[] addr, byte[] mask)
-		{
-			if(addr.Length!=4)
-				throw new ArgumentException("The array must have 4 bytes","addr");
-			if(mask.Length!=4)
-				throw new ArgumentException("The array must have 4 bytes","mask");
-			this.Address = addr;
-			this.Mask = mask;
-		}
-		
-		/// <summary>
-		/// Returns an string that represents the data.
-		/// </summary>
-		public string GetAddressAsString()
-		{
-			return Address[3]+"."+Address[2]+"."+Address[1]+"."+Address[0]+
-				(this.HasMask?
-					"/"+Mask[3]+"."+Mask[2]+"."+Mask[1]+"."+Mask[0]
-					:
-					String.Empty);
-		}
-		
-		/// <summary>
-		/// Returns an string that represents the IP address after the mask have
-		/// been applied over it. This is, the network address.
-		/// </summary>
-		public string GetMaskedAddressAsString()
-		{
-			return (Address[3] & Mask[3]) + "." + (Address[2] & Mask[2]) + "." +
-				(Address[1] & Mask[1]) + "." + (Address[0] & Mask[0]);
-		}
-		
-		/// <summary>
-		/// Returns an array that represents the IP address after the mask have
-		/// been applied over it. This is, the network address.
-		/// </summary>
-		public byte[] GetMaskedAddressAsArray()
-		{
-			return new byte[]{(byte)(Address[3] & Mask[3]) , (byte)(Address[2] & Mask[2]) ,
-				(byte)(Address[1] & Mask[1]) , (byte)(Address[0] & Mask[0])};
-		}
-		
-		/// <summary>
-		/// Sets the mask from the number of bits, starting to count in the msb, that
-		/// are set to 1 in the mask.
-		/// </summary>
-		public void SetMask(int numbits)
-		{
-			if(numbits<0 || numbits>32)
-				throw new ArgumentException("The number of bits in the mask are from 0 to 32");
-			
-			if(numbits<=8)
-				Mask = new byte[]{this.GetBitsValue(numbits),0,0,0};
-			else if(numbits<=16)
-				Mask = new byte[]{255,this.GetBitsValue(numbits-8),0,0};
-			else if(numbits<=24)
-				Mask = new byte[]{255,255,this.GetBitsValue(numbits-8),0};
-			else if(numbits<=32)
-				Mask = new byte[]{255,255,255,this.GetBitsValue(numbits-8)};
-		}
-		
-		/// <summary>
-		/// Convert a number of bits set to 1, starting in the msb of a group of
-		/// 8 bits, to a decimal value taking every bit not specified as 0.
-		/// </summary>
-		private byte GetBitsValue(int num)
-		{
-			if(num==8)
-				return 255;
-			else if(num==0)
-				return 0;
-			else if(num>8 || num<0)
-				throw new ArgumentException("The number of bits must be in the range 0-8","num");
-			
-			int max = 7;
-			int val = 0;
-			while(num>0)
-			{
-				val+= 2^(max--);
-				num--;
-			}
-			return Convert.ToByte(num);
-		}
-		
-		public SocketAddress AsSocketAddress()
-		{
-			SocketAddress sa = new SocketAddress(AddressFamily.InterNetwork, this.Address.Length);
-			for(int i=0;i<this.Address.Length;i++)
-				sa[i]=this.Address[i];
-			
-			return sa;
-		}
-		
-		/// <summary>
-		/// Converts a string that represents 4 octects separated by dots to an
-		/// array.
-		/// </summary>
-		public static byte[] ExtractOctects(string octectsAsString)
-		{
-			if(String.IsNullOrEmpty(octectsAsString))
-				throw new ArgumentException("The argument can't be null or empty string","octectsAsString");
-			else if(octectsAsString[0]=='.')
-				throw new FormatException("The first octect is empty!. Empty octects not allowed");
-			
-			int pos = 0;
-			int lastPos = 0;
-			List<byte> result = new List<byte>();
-			
-			while(pos>=0){
-				pos = octectsAsString.IndexOf('.',lastPos);
-				if(pos==lastPos)
-					throw new FormatException("Found empty octect!. Empty octects not allowed");
-				if(pos>=0)
-				{ //Get all bytes but the last one
-					result.Add(Byte.Parse(octectsAsString.Substring(lastPos, pos - lastPos)));
-					pos++;
-					lastPos = pos;
-				}
-				else
-				{ // Get the last byte
-					result.Add(Byte.Parse(octectsAsString.Substring(lastPos)));
-				}
-			}
-			return result.ToArray();					                                                
-		}
-		
-		/// <summary>
-		/// Parses an string as an IPv4 address. It allows mask specification.
-		/// </summary>
-		/// <remarks>
-		/// The formats allowed are:
-		/// - A.A.A.A/M.M.M.M
-		/// - A.A.A.A/M
-		/// - A.A.A.A
-		/// Where every letter between dots are values from 0 to 255 (an octect)
-		/// </remarks>
-		public static IPv4RawAddress Parse(string addr)
-		{
-			if(String.IsNullOrEmpty(addr))
-				throw new ArgumentException("Can't parse an empty or null string","addr");
-			
-			addr = addr.Trim();
-		    int pos = addr.IndexOf('/');
-		    
-		    IPv4RawAddress result = new IPv4RawAddress();
-
-	   	    if(pos>=0)
-  	        {
-				byte[] byteArr = ExtractOctects(addr.Substring(0,pos));
-				
-				if(byteArr.Length!=4)
-					throw new FormatException("The IPv4 address must have 4 octects");
-				
-				result.address = byteArr;
-				byteArr = ExtractOctects(addr.Substring(pos+1));
-				
-				//If we have only one byte is a short mask specification with the number
-				//of consecutive bits in the mask. IE: 192.168.1.1/17 so the mask is
-				//255.255.128.0
-				if(byteArr.Length==1)
-					result.SetMask(Convert.ToInt32(byteArr[0]));
-				else if(byteArr.Length==4)
-					result.mask = byteArr;
-				else
-					throw new FormatException("The mask has an invalid format. It can be an octect or 4 octects separated by dots.");
-	        }
-	        else
-	        {
-				byte[] byteArr = ExtractOctects(addr.Substring(0,pos));
-				
-				if(byteArr.Length!=4)
-					throw new FormatException("The IPv4 address must have 4 octects");
-				
-				result.address = byteArr;
-	        }
-		     
-		    return result;
-		}
-	}
+//	/// <summary>
+//	/// Raw data struct for ipv4. Stores ip and mask data.
+//	/// </summary>
+//	public struct IPv4RawAddress
+//	{
+//		/// <summary>
+//		/// Defines a IPv4RawAddress with all the bytes set to zero.
+//		/// </summary>
+//		public static readonly IPv4RawAddress Zero = new IPv4RawAddress(new byte[]{0,0,0,0},new byte[]{0,0,0,0});
+//		
+//		/// <summary>
+//		/// IP address (4 bytes)
+//		/// </summary>
+//		private byte[] address;
+//		
+//		/// <summary>
+//		/// Gets/Sets the address bytes (4 bytes).
+//		/// </summary>
+//		public byte[] Address
+//		{
+//			get {return this.address;}
+//			set {
+//				
+//				if(value.Length!=4)
+//					throw new ArgumentException("The address must be 4 bytes long","value");
+//				
+//				this.address = value;
+//			}
+//		}
+//		
+//		/// <summary>
+//		/// Mask (4bytes)
+//		/// </summary>
+//		private byte[] mask;
+//		
+//		/// <summary>
+//		/// Gets/Sets the mask bytes (4 bytes)
+//		/// </summary>
+//		public byte[] Mask
+//		{
+//			get {return this.mask;}
+//			set {
+//				
+//				if(value.Length!=4)
+//					throw new ArgumentException("The mask must be 4 bytes long","value");
+//				
+//				this.mask = value;
+//			}
+//		}
+//		
+//		/// <summary>
+//		/// Returns if there is a valid mask defined
+//		/// </summary>
+//		public bool HasMask
+//		{
+//			get { return Mask[3]==0 && Mask[2]==0 && Mask[1]==0 && Mask[0]==0;}
+//		}
+//		
+//		/// <summary>
+//		/// Initializes the struct with the bytes for the address.
+//		/// </summary>
+//		public IPv4RawAddress(byte[] addr)
+//		{
+//			if(addr.Length!=4)
+//				throw new ArgumentException("The array must have 4 bytes","addr");
+//			this.Address = addr;
+//			this.Mask = new byte[4]{0,0,0,0};
+//		}
+//		
+//		/// <summary>
+//		/// Initializes the struct with the bytes for the address and the mask
+//		/// </summary>
+//		public IPv4RawAddress(byte[] addr, byte[] mask)
+//		{
+//			if(addr.Length!=4)
+//				throw new ArgumentException("The array must have 4 bytes","addr");
+//			if(mask.Length!=4)
+//				throw new ArgumentException("The array must have 4 bytes","mask");
+//			this.Address = addr;
+//			this.Mask = mask;
+//		}
+//		
+//		/// <summary>
+//		/// Returns an string that represents the data.
+//		/// </summary>
+//		public string GetAddressAsString()
+//		{
+//			return Address[3]+"."+Address[2]+"."+Address[1]+"."+Address[0]+
+//				(this.HasMask?
+//					"/"+Mask[3]+"."+Mask[2]+"."+Mask[1]+"."+Mask[0]
+//					:
+//					String.Empty);
+//		}
+//		
+//		/// <summary>
+//		/// Returns an string that represents the IP address after the mask have
+//		/// been applied over it. This is, the network address.
+//		/// </summary>
+//		public string GetMaskedAddressAsString()
+//		{
+//			return (Address[3] & Mask[3]) + "." + (Address[2] & Mask[2]) + "." +
+//				(Address[1] & Mask[1]) + "." + (Address[0] & Mask[0]);
+//		}
+//		
+//		/// <summary>
+//		/// Returns an array that represents the IP address after the mask have
+//		/// been applied over it. This is, the network address.
+//		/// </summary>
+//		public byte[] GetMaskedAddressAsArray()
+//		{
+//			return new byte[]{(byte)(Address[3] & Mask[3]) , (byte)(Address[2] & Mask[2]) ,
+//				(byte)(Address[1] & Mask[1]) , (byte)(Address[0] & Mask[0])};
+//		}
+//		
+//		/// <summary>
+//		/// Sets the mask from the number of bits, starting to count in the msb, that
+//		/// are set to 1 in the mask.
+//		/// </summary>
+//		public void SetMask(int numbits)
+//		{
+//			if(numbits<0 || numbits>32)
+//				throw new ArgumentException("The number of bits in the mask are from 0 to 32");
+//			
+//			if(numbits<=8)
+//				Mask = new byte[]{this.GetBitsValue(numbits),0,0,0};
+//			else if(numbits<=16)
+//				Mask = new byte[]{255,this.GetBitsValue(numbits-8),0,0};
+//			else if(numbits<=24)
+//				Mask = new byte[]{255,255,this.GetBitsValue(numbits-8),0};
+//			else if(numbits<=32)
+//				Mask = new byte[]{255,255,255,this.GetBitsValue(numbits-8)};
+//		}
+//		
+//		/// <summary>
+//		/// Convert a number of bits set to 1, starting in the msb of a group of
+//		/// 8 bits, to a decimal value taking every bit not specified as 0.
+//		/// </summary>
+//		private byte GetBitsValue(int num)
+//		{
+//			if(num==8)
+//				return 255;
+//			else if(num==0)
+//				return 0;
+//			else if(num>8 || num<0)
+//				throw new ArgumentException("The number of bits must be in the range 0-8","num");
+//			
+//			int max = 7;
+//			int val = 0;
+//			while(num>0)
+//			{
+//				val+= 2^(max--);
+//				num--;
+//			}
+//			return Convert.ToByte(num);
+//		}
+//		
+//		public SocketAddress AsSocketAddress()
+//		{
+//			SocketAddress sa = new SocketAddress(AddressFamily.InterNetwork, this.Address.Length);
+//			for(int i=0;i<this.Address.Length;i++)
+//				sa[i]=this.Address[i];
+//			
+//			return sa;
+//		}
+//		
+//		/// <summary>
+//		/// Converts a string that represents 4 octects separated by dots to an
+//		/// array.
+//		/// </summary>
+//		public static byte[] ExtractOctects(string octectsAsString)
+//		{
+//			if(String.IsNullOrEmpty(octectsAsString))
+//				throw new ArgumentException("The argument can't be null or empty string","octectsAsString");
+//			else if(octectsAsString[0]=='.')
+//				throw new FormatException("The first octect is empty!. Empty octects not allowed");
+//			
+//			int pos = 0;
+//			int lastPos = 0;
+//			List<byte> result = new List<byte>();
+//			
+//			while(pos>=0){
+//				pos = octectsAsString.IndexOf('.',lastPos);
+//				if(pos==lastPos)
+//					throw new FormatException("Found empty octect!. Empty octects not allowed");
+//				if(pos>=0)
+//				{ //Get all bytes but the last one
+//					result.Add(Byte.Parse(octectsAsString.Substring(lastPos, pos - lastPos)));
+//					pos++;
+//					lastPos = pos;
+//				}
+//				else
+//				{ // Get the last byte
+//					result.Add(Byte.Parse(octectsAsString.Substring(lastPos)));
+//				}
+//			}
+//			return result.ToArray();					                                                
+//		}
+//		
+//		/// <summary>
+//		/// Parses an string as an IPv4 address. It allows mask specification.
+//		/// </summary>
+//		/// <remarks>
+//		/// The formats allowed are:
+//		/// - A.A.A.A/M.M.M.M
+//		/// - A.A.A.A/M
+//		/// - A.A.A.A
+//		/// Where every letter between dots are values from 0 to 255 (an octect)
+//		/// </remarks>
+//		public static IPv4RawAddress Parse(string addr)
+//		{
+//			if(String.IsNullOrEmpty(addr))
+//				throw new ArgumentException("Can't parse an empty or null string","addr");
+//			
+//			addr = addr.Trim();
+//		    int pos = addr.IndexOf('/');
+//		    
+//		    IPv4RawAddress result = new IPv4RawAddress();
+//
+//	   	    if(pos>=0)
+//  	        {
+//				byte[] byteArr = ExtractOctects(addr.Substring(0,pos));
+//				
+//				if(byteArr.Length!=4)
+//					throw new FormatException("The IPv4 address must have 4 octects");
+//				
+//				result.address = byteArr;
+//				byteArr = ExtractOctects(addr.Substring(pos+1));
+//				
+//				//If we have only one byte is a short mask specification with the number
+//				//of consecutive bits in the mask. IE: 192.168.1.1/17 so the mask is
+//				//255.255.128.0
+//				if(byteArr.Length==1)
+//					result.SetMask(Convert.ToInt32(byteArr[0]));
+//				else if(byteArr.Length==4)
+//					result.mask = byteArr;
+//				else
+//					throw new FormatException("The mask has an invalid format. It can be an octect or 4 octects separated by dots.");
+//	        }
+//	        else
+//	        {
+//				byte[] byteArr = ExtractOctects(addr.Substring(0,pos));
+//				
+//				if(byteArr.Length!=4)
+//					throw new FormatException("The IPv4 address must have 4 octects");
+//				
+//				result.address = byteArr;
+//	        }
+//		     
+//		    return result;
+//		}
+//	}
 	
-	 /// <summary>
+	/// <summary>
     /// Defines a port range that at least can be a range of 1 port.
     /// </summary>
     public struct PortRange
@@ -280,8 +280,7 @@ namespace Developer.Common.Net
         private short startPort;
         
 		/// <summary>
-		/// Start port of the range. If the SinglePort property is true this
-		/// has the same value as EndPort.
+		/// Start port of the range.
 		/// </summary>
         public short StartPort
         {
@@ -291,8 +290,7 @@ namespace Developer.Common.Net
         private short endPort;
         
 		/// <summary>
-		/// End port of the range. If the SinglePort property is true this
-		/// has the same value as EndPort.
+		/// End port of the range.
 		/// </summary>
         public short EndPort
         {
@@ -330,7 +328,13 @@ namespace Developer.Common.Net
 		/// </summary>
         public override string ToString ()
         {
-            return startPort+":"+endPort;
+			//We return a string formatted for each case
+			if(startPort == endPort)
+				return startPort.ToString();
+			else if(startPort == 0)
+				return ":"+endPort;
+			else
+				return startPort+":"+endPort;
         }
 		
 		/// <summary>
@@ -372,36 +376,52 @@ namespace Developer.Common.Net
             short startp;
 			
             int pos = range.IndexOf (':');
+			//Console.WriteLine("Parsing range: "+range+" with char at pos: "+pos);
             
             if (pos < 0) {
+				//Console.WriteLine("pos<0");
                 //Only first port
                 if (!Int16.TryParse(range, out startp)){
                     throw new InvalidCastException("PortRange::Parse(string): Can't "+
                                                    "parse to integer. Data: "+range);
                 }
-                endp = Int16.MaxValue;
-				startp = endp;
+				//One port range
+                endp = startp;
             } else if (pos == 0) {
+				//Console.WriteLine("pos == 0");
                 //Only last port
                 if (!Int16.TryParse(range.Substring(1), out endp)){
                     throw new InvalidCastException("PortRange::Parse(string): Can't "+
                                                    "parse to integer. Data: "+range);
                 }
-                startp = 0;
+				//The range starts in zero
+				startp = 0;
+			} else if (pos == (range.Length - 1)){
+				throw new InvalidCastException("PortRange::Parse(string): Can't parse end port");
             } else {
+				//Console.WriteLine("pos > 0");
 				//Convert the first port
 				if (!Int16.TryParse (range.Substring(0,pos),out startp)){
                     throw new InvalidCastException("PortRange::Parse(string): Can't "+
                                                    "parse start port to integer. Data: "+range);				
 				}
 				//Convert the last port
-				if (!Int16.TryParse (range.Substring(0,pos),out endp)){
+				if (!Int16.TryParse (range.Substring(pos+1),out endp)){
                     throw new InvalidCastException("PortRange::Parse(string): Can't "+
                                                    "parse end port to integer. Data: "+range);				
 				}
 			}
-            
+
+			//Swap port if are not in order
+			if(startp>endp) {
+				short temp = startp;
+				startp = endp;
+				endp = temp;
+			}
+			
+            Console.Out.WriteLine("Found ports ["+startp+", "+endp+"]");
             return new PortRange (startp, endp);
         }
     }
 }
+						
