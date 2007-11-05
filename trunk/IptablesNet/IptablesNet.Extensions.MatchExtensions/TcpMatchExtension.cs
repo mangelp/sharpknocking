12ce7adccce52f853d09b9d6ebc55e5267d238b1
mangelp <mangelp@26a6a90b-202d-0410-bbe6-95a292849a38>
2007-11-05 17:32:00 +0000

    - Resolved some bugs about getting the rules in iptables-save format. - Solved bugs in state extension that gived back an exception when creating the parameters. - Updated license headers and namespaces.

--- trunk/IptablesNet/IptablesNet.Extensions.MatchExtensions/TcpMatchExtension.cs
+++ trunk/IptablesNet/IptablesNet.Extensions.MatchExtensions/TcpMatchExtension.cs
@@ -1,3 +1,23 @@
+// TcpMatchExtension.cs
+//
+//  Copyright (C) 2007 iSharpKnocking project
+//  Created by Miguel Angel Perez (mangelp{@}gmail{d0t}com)
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+//
+//
 
 using System;
 using System.Text;
@@ -121,15 +141,13 @@ namespace IptablesNet.Extensions.Matches
             
             protected override string GetValuesAsString ()
             {
-                return this.range.ToString();
+				return range.ToString();
             }
             
             public override void SetValues (string value)
             {
                 this.range = PortRange.Parse(value);
             }
-
-
         }
 		
         /// <summary>
