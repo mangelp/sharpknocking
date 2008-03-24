be9bb6aa73d44ed533b1ac765a2f87eef4354914
mangelp <mangelp@26a6a90b-202d-0410-bbe6-95a292849a38>
2008-03-24 15:29:37 +0000

    Everything is still very unstable :( sigh!

--- Tests/skdaemonControl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This test only works if it is run from the current directory and every
-# configuration file needed is in the directory.
-
-EXECPATH=" ../SharpKnocking/KnockingDaemonControl/bin/Debug/KnockingDaemonControl.exe"
-ARGS=""
-
-mono $EXECPATH $ARGS
-
