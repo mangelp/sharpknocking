3aa48a8809a36f893b7983c88a2b5c4bcc60ce90
mangelp <mangelp@26a6a90b-202d-0410-bbe6-95a292849a38>
2008-08-15 12:01:38 +0000

    - Some changes to the bindings.

--- trunk/Extras/IptablesSharp/IptablesSharp.Core/Bindings/IptablesCmdBinding.cs
+++ trunk/Extras/IptablesSharp/IptablesSharp.Core/Bindings/IptablesCmdBinding.cs
@@ -1,6 +1,7 @@
 // IptablesCmdBinding.cs
 //
-//  Copyright (C) 2008 [name of author]
+//  Copyright (C) 2008 iSharpKnocking project
+//  Created by Miguel Angel Perez <mangelp>at<gmail>*<com>
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
